b9eed3a627ba217feb7a2692dc8f561f90b8b2ea
Isaac Valdez <isaac_valdez@msn.com>
2024-03-23 21:48:13 -0500

    v0.0.0.2

--- flatten_markdown/__main__.py
+++ flatten_markdown/__main__.py
@@ -1,41 +1,24 @@
-import os
-from pathlib import Path
-import typer
+import argparse
+from flatten_markdown.core import process_directory, process_file_flatten, process_file_unflatten
 
-app = typer.Typer()
+from flatten_markdown.utils import read_file, write_file
 
-def read_markdown_file(file_path):
-    with open(file_path, 'r', encoding='utf-8') as file:
-        return file.read()
 
-def write_markdown_file(file_path, content):
-    with open(file_path, 'w', encoding='utf-8') as file:
-        file.write(content)
 
-def flatten_markdown_files(directory, output_file, base_level=1):
-    output_content = ""
+def main():
+    parser = argparse.ArgumentParser(description="Flatten or unflatten Markdown files.")
+    parser.add_argument('action', choices=['flatten', 'unflatten'], help="Action to perform")
+    parser.add_argument('input', help="Input directory or file")
+    parser.add_argument('output', help="Output file or directory")
+    args = parser.parse_args()
 
-    for item in os.listdir(directory):
-        item_path = Path(directory) / item
-
-        if item_path.is_file() and item_path.suffix == '.md':
-            content = read_markdown_file(item_path)
-            heading_level = '#' * base_level
-            output_content += f"{heading_level} {item_path.stem}\n\n{content}\n\n"
-
-        elif item_path.is_dir():
-            heading_level = '#' * base_level
-            output_content += f"{heading_level} {item}\n\n"
-            output_content += flatten_markdown_files(item_path, output_file, base_level + 1)
-
-    return output_content
-
-@app.command()
-def main(directory: str = typer.Argument('.', help="Directory to flatten")):
-    output_file = 'flattened_markdown.md'
-    flattened_content = flatten_markdown_files(directory, output_file)
-    write_markdown_file(output_file, flattened_content)
-    print(f"Flattened Markdown file created: {output_file}")
+    if args.action == 'flatten':
+        flattened_content = process_directory(args.input, process_file_flatten, args.output)
+        write_file(args.output, flattened_content)
+        print(f"Flattened Markdown file created: {args.output}")
+    elif args.action == 'unflatten':
+        process_file_unflatten(args.input, args.output)
+        print(f"Unflattened Markdown files created in: {args.output}")
 
 if __name__ == "__main__":
-    app()
+    main()
---
+++ tests/__init__.py
@@ -0,0 +1,10 @@
+from tests.test_flatten import test_flatten
+from tests.test_unflatten import test_unflatten
+from tests.test_flatten_unflatten import test_flatten_unflatten
+
+__all__ = [
+    'test_flatten',
+    'test_unflatten',
+    'test_flatten_unflatten'
+]
+
